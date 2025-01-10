b6844f3d5cc6a882a59c75f368fc2e741eb5e9cd
a2c <a2cgle@gmail.com>
2025-01-10 13:49:28 +0900

    wip

--- step2_make-ssh-key.sh
+++ step2_make-ssh-key.sh
@@ -6,6 +6,9 @@ setup_ssh() {
     username=$(whoami)
     hostname=$(hostname)
     ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/${username}_${hostname} -N ""
+
+    ls -la ~/.ssh
+
     #ssh-copy-id -i "~/.ssh/${username}_${hostname}.pub" user@your-ssh-server
     echo "Host your-ssh-server" >> ~/.ssh/config
     echo "  User yourusername" >> ~/.ssh/config
